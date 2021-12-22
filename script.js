c93bb8f9405c9f8420926e184c9de3e70be1507c
Emily Curran <1303currane@gmail.com>
2021-12-22 19:20:05 +0000

    mostly finished, just needs some refactoring and css changes

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-var mode = "black";
+var colourMode = "black";
 var gridSize = 17;
-var myGrid = createGrid(gridSize);
-myGrid.setAttribute('id', 'grid');
-var thisContainer = document.getElementById('container');
-thisContainer.appendChild(myGrid);
 
-var squares = Array.from(document.querySelectorAll('.square'));
-squares.forEach(square => square.addEventListener('mouseenter', changeSquareColour));
+const resetButton = document.getElementById('reset-button');
+const rainbowButton = document.getElementById('rainbow-button');
+const blackButton = document.getElementById('black-button');
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+
+output.innerHTML = slider.value;
+
+
+resetButton.addEventListener('click', resetGrid);
+rainbowButton.addEventListener('click', changeColourModeToRainbow);
+blackButton.addEventListener('click', changeColourModeToBlack)
+
+newGrid = createGrid(gridSize);
+myGrid = newGrid;
+myGrid.setAttribute('id', 'grid');
+document.getElementById('container').appendChild(myGrid);
+console.log("here");
+console.log(myGrid);
+console.log("here2");
+var test = document.getElementById('grid');
+console.log(test);
+addGridToHTML(newGrid, gridSize);
 
 function changeSquareColour(){
-    this.style.backgroundColor = 'black';
+    console.log("insider changeSquareColour");
+    console.log(myGrid);
+    if(colourMode == "black"){
+        this.style.backgroundColor = 'black';
+    }else if(colourMode == "rainbow"){
+        this.style.backgroundColor = chooseRandomColour();
+    }
+}
+
+function chooseRandomColour(){
+    var r = Math.floor(Math.random()*257);
+    var g = Math.floor(Math.random()*257);
+    var b = Math.floor(Math.random()*257);
+    return "rgb("+r+", "+g+", "+b+")";
 }
 
 function createGrid(gridSize){
     //create a grid of divs with gridSize x gridSize number of elements
+    //note that only creates the div, doesn't add it to page
+    console.log(gridSize);
     var grid = document.createElement('div');
     for(let i=0; i<gridSize; i++){
         var row = document.createElement('div');
@@ -21,11 +53,62 @@ function createGrid(gridSize){
         for(var j=0; j<gridSize; j++){
             var square = document.createElement("div");
             square.className = "square";
-            console.log(typeof(square));
             row.appendChild(square);
         }
         grid.appendChild(row);
     }
+    grid.setAttribute('class', gridSize);
     return grid;
 }
 
+function resetGrid(){
+    var squares = Array.from(document.querySelectorAll('.square'));
+    squares.forEach(square => square.style.backgroundColor = 'white');
+}
+
+function changeColourModeToRainbow(){
+    colourMode = "rainbow";
+}
+
+function changeColourModeToBlack(){
+    colourMode = "black";
+}
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    gridSize = this.value;
+    newGrid = createGrid(gridSize);
+    addGridToHTML(newGrid, gridSize);
+  }
+
+function addGridToHTML(newGrid, gridSize){
+    console.log("start of addGridToHTML");
+    console.log(newGrid);
+
+    //remove the old grid from the html page
+    var oldGrid = document.getElementById('grid');
+    oldGrid.remove();
+
+    
+
+    //finish initialising newGrid
+    newGrid.setAttribute('id', 'grid');
+    myGrid = newGrid;
+    console.log(myGrid);
+
+
+    //insert the newly created grid in its place
+    var container = document.getElementById('container');
+    container.appendChild(newGrid);   
+    initialiseSquares(); 
+}
+
+function initialiseSquares(){
+    var squares = Array.from(document.querySelectorAll('.square'));
+    squares.forEach(square => square.addEventListener('mouseenter', changeSquareColour));
+
+}
+
+function start(){
+    createGrid()
+}
